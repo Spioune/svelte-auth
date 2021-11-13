b27f0894f3a7e549e1389eeb27ad662406d5fb5c
Spioune <>
2021-11-13 18:45:15 +0100

    fix: test en var

--- src/routes/api/auth/test.js
+++ src/routes/api/auth/test.js
@@ -1,6 +1,6 @@
 export async function get({ headers, method, host, path, query, body, params }) {
 	return {
 		status: 200,
-		body: JSON.stringify({ url: process.env.URL, base_url: AUTH0_BASE_URL }),
+		body: JSON.stringify({ url: process.env.URL, base_url: process.env.AUTH0_BASE_URL }),
 	}
 }
